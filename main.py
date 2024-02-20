9abb4a4d7f906fe4525f6da7c566d66d0cdec7b5
Tony Frietto <tony@n3-79-236.dhcp.drexel.edu>
2024-02-20 13:04:43 -0500

    added observation space enhancement

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 from pettingzoo.classic import texas_holdem_no_limit_v6
 
-env = texas_holdem_no_limit_v6.env(render_mode="human")
+env = texas_holdem_no_limit_v6.env(render_mode="human", num_players=8)
 env.reset(seed=42)
+
+def print_obs(obs):
+    i = 0
+    for item in observation['observation']:
+        if i <= 12:
+            print('spades '+str(i)+': '+str(item))
+        elif i <= 25:
+            print('hearts '+str(i % 13)+': '+str(item))
+        elif i <= 38:
+            print('diamonds '+str(i % 13)+': '+str(item))
+        elif i <= 51:
+            print('clubs '+str(i % 13)+': '+str(item))
+        elif i <= 59:
+            print('player '+str(i % 51)+' chips: '+str(item))
+        elif i == 60:
+            print('total pot: '+str(item))
+        elif i <= 68:
+            print('total bets player '+str(i % 60)+': '+str(item))
+        i += 1
+    print('----------------------------------------------------------------')
 
 for agent in env.agent_iter():
     observation, reward, termination, truncation, info = env.last()
+    print_obs(observation)
 
     if termination or truncation:
         action = None
