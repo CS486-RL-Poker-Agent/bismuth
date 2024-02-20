9e5999a467b1321255a2296c2004536ded049246
Tristan Wainwright <wainwright@disroot.org>
2024-02-20 14:22:27 -0500

    stubbing for policy parameter update

--- agent.py
+++ agent.py
@@ -1,3 +1,6 @@
+from typekit import SAR
+
+
 class Agent:
     def __init__(self, alpha: float, gamma: float) -> None:
         self.alpha = alpha
@@ -5,8 +8,9 @@ class Agent:
 
 
 class REINFORCEAgent(Agent):
-    def __init__(self, alpha: float, gamma: float) -> None:
+    def __init__(self, alpha: float, gamma: float, theta=[0]) -> None:
         super().__init__(alpha, gamma)
+        self.theta = theta
 
     def computeReturnValue(self, t: int, T: int, rewards: list[int]) -> int:
         G = 0
@@ -14,6 +18,14 @@ class REINFORCEAgent(Agent):
             G += self.gamma ** (k - t - 1) * rewards[k]
         return G
 
-    def REINFORCE(self, T: int, rewards: list[int]):
+    def updatePolicyParameters(self, t: int, G: int, state):
+        for parameter in self.theta:
+            parameter = self.alpha * self.gamma ** t * G
+
+    def REINFORCE(self, T: int, steps: list[SAR]):
+        states = [step["state"] for step in steps]
+        actions = [step["action"] for step in steps]
+        rewards = [step["reward"] for step in steps]
         for t in range(T):
             G = self.computeReturnValue(t, T, rewards)
+            self.updatePolicyParameters(t, G, states[t])
