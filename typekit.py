33a4759d7447276b362bf54b98ad5b7f23ac6e96
Tristan Wainwright <wainwright@disroot.org>
2024-02-20 14:21:54 -0500

    add custom type file

---
+++ typekit.py
@@ -0,0 +1,9 @@
+from typing import TypedDict, Any
+
+
+class SAR(TypedDict):
+    # TODO: Define this type
+    state: Any | None
+    # TODO: Define this type
+    action: Any | None
+    reward: float
