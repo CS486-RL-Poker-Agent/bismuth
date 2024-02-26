abdf51e087f38725057026b7335351556f95fa5d
Tristan Wainwright <wainwright@disroot.org>
2024-02-26 16:08:46 -0500

    clean code formating

--- project/training/heads_up.py
+++ project/training/heads_up.py
@@ -4,6 +4,9 @@ from custom_types import Step
 
 
 def generate_episode(episode_agent: REINFORCEAgent):
+    env = texas_holdem_no_limit_v6.env(render_mode="human")
+    env.reset(seed=42)
+
     T = 0
     steps: list[Step] = []
     for agent in env.agent_iter():
@@ -13,26 +16,25 @@ def generate_episode(episode_agent: REINFORCEAgent):
             action = None
         else:
             mask = observation["action_mask"]
-            # this is where you would insert your policy
             action = env.action_space(agent).sample(mask)
 
         if (agent == episode_agent.getName()):
             steps.append({"state": observation, "action": action})
             if steps[-1]:
                 steps[-1]["reward"] = reward
-        print("Current steps for player_0:", steps)
+
         env.step(action)
         T += 1
     episode_agent.REINFORCE(T, steps)
     env.close()
 
 
-if __name__ == "__main__":
-    env = texas_holdem_no_limit_v6.env(render_mode="human")
-    env.reset(seed=42)
-
-    epochs = 100
+def train():
     agent = REINFORCEAgent(0.01, 0.99)
-
-    for _ in range(epochs):
+    for _ in range(100):
         generate_episode(agent)
+    print("Final parameterized policy:", agent.get_theta())
+
+
+if __name__ == "__main__":
+    train()
