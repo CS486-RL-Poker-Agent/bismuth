61dc8e6f4ec92b423ea869dbdab80b83e85dac37
Tristan Wainwright <wainwright@disroot.org>
2024-03-09 15:10:05 -0500

    implement proper probability dist masking

--- policy.py
+++ policy.py
@@ -1,35 +1,33 @@
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch import Tensor
 from torch.distributions import Categorical
 
-from constants import OBSERVATION_SPACE_SIZE, ACTION_SPACE_SIZE
+from constants import CPU
+
+
+class CategoricalMasked(Categorical):
+    def __init__(self, logits: torch.Tensor, mask: torch.Tensor) -> None:
+        ninf = torch.tensor(torch.finfo(logits.dtype).min, dtype=logits.dtype)
+        logits = torch.where(mask, logits, ninf)
+        super(CategoricalMasked, self).__init__(logits=logits)
 
 
 class Policy(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, observation_space_size: int, action_space_size: int, hidden_layer_size: int) -> None:
         super(Policy, self).__init__()
-        self.fc1 = nn.Linear(OBSERVATION_SPACE_SIZE, 24)
-        self.fc2 = nn.Linear(24, ACTION_SPACE_SIZE)
+        self.fc1 = nn.Linear(observation_space_size, hidden_layer_size)
+        self.fc2 = nn.Linear(hidden_layer_size, hidden_layer_size * 2)
+        self.fc3 = nn.Linear(hidden_layer_size * 2, action_space_size)
 
-    def forward(self, x: Tensor) -> Tensor:
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.fc2(x)
-        return F.softmax(x, dim=1)
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
 
-    def act(self, state, mask) -> tuple:
-        # TODO: Make .to(device) programmatic
-        state = torch.from_numpy(state).float().unsqueeze(0).to("cpu")
-        probs = self.forward(state).cpu()
-        m = Categorical(probs)
+    def act(self, state, mask: list[int]):
+        state = torch.from_numpy(state).float().unsqueeze(0).to(CPU)
+        logits = self.forward(state).cpu()
+        m = CategoricalMasked(logits, torch.tensor(mask, dtype=torch.bool))
         action = m.sample()
-        print("act: ", action)
-        if mask[action] == 0:
-            action = np.random.randint(0,len(mask)-1)
-            while(mask[action] == 0):
-                action = np.random.randint(0,len(mask)-1)
-            action = torch.tensor(action)
-        return (action.item(), m.log_prob(action))
+        return action.item(), m.log_prob(action)
